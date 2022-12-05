e22b35bba77a5e8c3f767f245a70d3353357447b
Julian Schmid <68326859+KingNexu@users.noreply.github.com>
2022-12-05 22:15:10 +0100

    🧝 Day #4

---
+++ AdventOfCodeDay_4/adventOfCodeDay4.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Elf struct {
+	startPos int64
+	endPos   int64
+}
+
+func containsFullRange(elf1 Elf, elf2 Elf) bool {
+	if elf1.startPos >= elf2.startPos && elf1.endPos <= elf2.endPos {
+		return true
+	} else if elf2.startPos >= elf1.startPos && elf2.endPos <= elf1.endPos {
+		return true
+	} else {
+		return false
+	}
+}
+
+func containsAnyRange(elf1 Elf, elf2 Elf) bool {
+	if elf1.startPos >= elf2.startPos && elf1.startPos <= elf2.endPos {
+		return true
+	} else if elf2.startPos >= elf1.startPos && elf2.startPos <= elf1.endPos {
+		return true
+	} else {
+		return false
+	}
+}
+
+func problem() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("❌", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	elfRangCounter := 0
+	elfAnyCounter := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		splitLine := strings.Split(line, ",")
+
+		var elfSlice []Elf
+		for _, v := range splitLine {
+			posSlice := strings.Split(v, "-")
+			startPosSlice, err := strconv.ParseInt(posSlice[0], 10, 16)
+			endPosSlice, err := strconv.ParseInt(posSlice[1], 10, 16)
+			if err != nil {
+				log.Fatal("❌: ", err)
+			}
+			elfSlice = append(elfSlice, Elf{startPosSlice, endPosSlice})
+		}
+
+		// PROBLEM 1
+		if containsFullRange(elfSlice[0], elfSlice[1]) == true {
+			elfRangCounter++
+		}
+
+		// PROBLEM 2
+		if containsAnyRange(elfSlice[0], elfSlice[1]) == true {
+			elfAnyCounter++
+		}
+	}
+	fmt.Printf("Problem 1: %v\nProblem2: %v\n", elfRangCounter, elfAnyCounter)
+
+}
+
+func main() {
+	problem()
+
+}
